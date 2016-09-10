3cab52189d267f85398d7f1816fac22a68080ebf
Samir <samirshah@deloitte.com>
2016-09-10 14:38:14 +0530

    First Commit

---
+++ app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from 'angular2/core';
+import { HTTP_PROVIDERS } from 'angular2/http';
+import 'rxjs/Rx';   // Load all features
+import { ROUTER_PROVIDERS, RouteConfig, ROUTER_DIRECTIVES } from 'angular2/router';
+
+import { WelcomeComponent } from './home/welcome.component';
+
+@Component({
+    selector: 'pm-app',
+    template: `
+    <div>
+        <nav class='navbar navbar-default'>
+            <div class='container-fluid'>
+                <a class='navbar-brand'>{{pageTitle}}</a>
+                <ul class='nav navbar-nav'>
+                    <li><a [routerLink]="['Welcome']">Home</a></li>
+                    <li><a [routerLink]="['EmptyState']">Empty State</a></li>
+                    <li><a [routerLink]="['Accordion']">Accordion</a></li>
+                    <li><a [routerLink]="['Pagination']">Pagination</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class='container'>
+            <router-outlet></router-outlet>
+        </div>
+     </div>
+    ` ,
+    directives: [ROUTER_DIRECTIVES],
+    providers: [HTTP_PROVIDERS,
+                ROUTER_PROVIDERS]
+})
+@RouteConfig([
+  { path: '/welcome', name: 'Welcome', component: WelcomeComponent, useAsDefault: true }
+])
+export class AppComponent {
+    pageTitle: string = 'Angular2 Components';
+}
